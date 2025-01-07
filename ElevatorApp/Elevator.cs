b771311b8c14a1dae29750f33dc13b0130bac1de
Projkeen Theone <projkeen@outlook.com>
2025-01-07 19:45:54 +0300

    Добавьте файлы проекта.

---
+++ ElevatorApp/Elevator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorApp
+{
+    public enum ElevatorStatus
+    {
+        Free, Busy, MovingToFirstFloor
+    }
+    public class Elevator
+    {
+        public int Id {  get; set; }
+        public ElevatorStatus Status { get; set; }
+        public int Position {  get; set; }
+
+        public Elevator(int numberOfElevator)
+        {
+            Id = numberOfElevator;
+        }
+
+        public void SetRandomStatus(Random random)
+        {
+            Array statuses = Enum.GetValues(typeof(ElevatorStatus));
+            Status = (ElevatorStatus)statuses.GetValue(random.Next(statuses.Length));
+        }
+
+        public void SetRandomPositions(Random random)
+        {
+            Position = random.Next(1, 26);
+        }
+    }
+    
+}
---
+++ ElevatorApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using ElevatorApp;
+using System;
+
+List<Elevator> elevators = new List<Elevator>
+            {
+                new Elevator(1),
+                new Elevator(2),
+                new Elevator(3)
+            };
+Random random = new Random();
+foreach (var elevator in elevators)
+{
+    elevator.SetRandomStatus(random);
+    elevator.SetRandomPositions(random);
+}
+Console.WriteLine("Positions of elevators:");
+foreach (var elevator in elevators)
+{
+    Console.WriteLine("Elevator " + elevator.Id +": Status - " + elevator.Status +", Position - " + elevator.Position + " floor");
+}
+Console.WriteLine();
+Console.WriteLine("Push <Enter> for call elevator:");
+Console.ReadLine();
+Elevator nearestElevator = CallNearestElevator();
+Console.WriteLine($"Elevator №{nearestElevator.Id} ({nearestElevator.Status}) is coming");
+
+Elevator CallNearestElevator()
+{    
+    var availableElevators = elevators.Where(e => e.Status == ElevatorStatus.Free ||
+                                                  e.Status == ElevatorStatus.MovingToFirstFloor).ToList();
+    if (availableElevators.Count == 0)
+        return null;    
+    var movingToTheFirstFloorElevators = availableElevators.Where(e => e.Status == ElevatorStatus.MovingToFirstFloor)
+                                                         .OrderBy(e => e.Position).ToList();
+    if (movingToTheFirstFloorElevators.Count > 1)
+        
+        return movingToTheFirstFloorElevators[0];
+    return availableElevators.OrderBy(e => e.Position).First();
+}
